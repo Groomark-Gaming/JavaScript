2b349717a7c0781fc2f475062623bfef223f410a
Groomark-Gaming <harshitgawade755@gmail.com>
2026-08-29 09:48:26 +0000

    changes

--- 1_basics/DataTypes-Summary.js
+++ 1_basics/DataTypes-Summary.js
@@ -32,5 +32,35 @@ console.log(typeof bigNum); //for null it will show object
                             //for object it shows function
 console.log(heros);
 console.log(myObj);
+console.log();
+
+console.log("++++++++++++++MEMORY++++++++++++++++");
+
+console.log();
+
+//memory : Stack (Primitive), Heap(Non-Primitive)
+
+//primitive -> Stack -> Get Copy 
+let myYTname = "Groomark Gaming"
+let anotherName = myYTname
+anotherName = "YT GROOMARK 619"
+
+console.log(myYTname);
+console.log(anotherName);
+
+let userOne = {
+    Email:"123@gmail.com",
+    UPI:"user@ybl"
+}
+
+//Non Primitive -> Heap -> Referance : we are making changes in original value
+let userTwo = userOne
+userTwo.email = "Groomark@gmail.com"
+userTwo.UPI = "GROOMARKGG@ybl"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
 
 
