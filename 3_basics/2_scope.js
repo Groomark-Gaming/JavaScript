979069e24a66fed01316339f2522c8d39c44c2ab
Groomark-Gaming <harshitgawade755@gmail.com>
2026-09-03 15:58:25 +0000

    Scope

--- 3_basics/2_scope.js
+++ 3_basics/2_scope.js
@@ -11,3 +11,48 @@ if(true){
 console.log(a);    //gives error
 // console.log(b);    gives error
 console.log(c);       //works perfectly if we dont add var then still it works
+console.log();
+
+function one(){
+    const userName = "GROOMARK"
+
+    function two(){
+        const website = "YOUTUBE"
+        console.log(userName);
+        
+    }
+    // console.log(website);    shows error
+    two()
+}
+one()
+
+
+if (true) {
+    const username = "GROOMARK"
+    if (username==="GROOMARK") {
+        const web = " YOUTUBE"
+        console.log(username + web);
+        
+    }
+    // console.log(web);    error because out of scope 
+    
+}
+// console.log(username);   error because out of scope 
+
+console.log();
+console.log("++++++++++++++ INTRESTING ++++++++++++++++");
+console.log();
+
+// addOne(4)    we can write this here also
+
+function addOne(num) {
+    return num +1
+}
+console.log(addOne(4));
+
+//but we cant write addTwo(5) at begining because of declaration of variable
+
+const addTwo = function(num){
+    return num + 2
+}
+console.log(addTwo(5));
